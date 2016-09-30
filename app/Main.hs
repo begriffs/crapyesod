b8ce2fdbb8503edef41d0a89599c90936726cbbb
Joe Nelson <cred+github@begriffs.com>
2016-09-29 21:29:41 -0700

    Interpolate string in response

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import Lib
+import Network.HTTP.Simple
 
-main :: IO ()
-main = someFunc
+import Lib
+import Yesod
+
+data Piggies = Piggies
+
+instance Yesod Piggies
+
+mkYesod "Piggies" [parseRoutes|
+  / HomeR GET
+|]
+
+getHomeR =
+  let fun = "hi"::String in
+  defaultLayout $ do
+    setTitle "Minimal Multifile"
+    [whamlet|
+      #{fun},
+      Welcome to the Pigsty!
+    |]
+
+main = warpDebug 3000 Piggies
